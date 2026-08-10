e845344f55e7b8e09b5c0f69039eabddf8039917
Mandeep <bullet1128@gmail.com>
2026-08-10 00:17:39 -0500

    changed case temporarily

--- PageParts.js
+++ PageParts.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("footer.html")
+    fetch("Footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
