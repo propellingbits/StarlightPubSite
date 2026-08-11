f8a543e72aeedfc1fadf58e7c06b9e0043557ecd
Mandeep <bullet1128@gmail.com>
2026-08-10 21:26:23 -0500

    1. added defer 2. Case corrected  'footer'

--- PageParts.js
+++ PageParts.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("Footer.html")
+    fetch("footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
