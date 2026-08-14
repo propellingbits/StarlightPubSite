db7961c92d5f1bf9f7764497390516c950b4326f
Mandeep <bullet1128@gmail.com>
2026-08-13 20:28:49 -0500

    added analytics

--- PageParts.js
+++ PageParts.js
@@ -5,7 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
         });
+    googleAnalytics();
 
+    //MS clarity
+    (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y20gthhmip");
+    
     // loading components.js
     /*document.querySelectorAll('script[type="text/x-dc"]').forEach(async (scriptTag) => {
         const scriptUrl = scriptTag.getAttribute('data-dc-script');
@@ -28,3 +36,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });*/
 
 });
+
+function googleAnalytics() {
+    
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-3NNPMHP6Y3');    
+}
